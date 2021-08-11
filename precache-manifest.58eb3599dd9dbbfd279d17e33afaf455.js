b9cf0e29ac01f0bf8ac8f97a4c5cbadce71172df
dhaval-76 <solankidhavals982@gmail.com>
2021-08-11 09:09:35 +0530

    Updates

---
+++ precache-manifest.58eb3599dd9dbbfd279d17e33afaf455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc051cd6e9527271a78a5b6da99087d7",
+    "url": "/Recipe-Suggestion-App/index.html"
+  },
+  {
+    "revision": "3378471c995027d8c66c",
+    "url": "/Recipe-Suggestion-App/static/css/main.339a0d1c.chunk.css"
+  },
+  {
+    "revision": "989d530ec06a8b6650ff",
+    "url": "/Recipe-Suggestion-App/static/js/2.e7e8f19c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-Suggestion-App/static/js/2.e7e8f19c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3378471c995027d8c66c",
+    "url": "/Recipe-Suggestion-App/static/js/main.787cc16e.chunk.js"
+  },
+  {
+    "revision": "a32b0e0751557d082267",
+    "url": "/Recipe-Suggestion-App/static/js/runtime-main.291a330b.js"
+  }
+]);
